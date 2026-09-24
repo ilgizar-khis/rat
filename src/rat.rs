290059390cdf033e620a7572ef80c123e01f6cf5
razigli <razigli@artix.dev>
2026-09-24 13:56:50 +0500

    create get_width/height functions and calc

--- src/rat.rs
+++ src/rat.rs
@@ -1,3 +1,5 @@
+use ratatui::layout::Rect;
+
 pub struct Rat {
     pos: [u16; 2],
     size: u16,
@@ -10,6 +12,23 @@ impl Rat {
             pos: [pos[0] * 2, pos[1]],
             size,
             field: [field[0] * 2, field[1]],
+        }
+    }
+
+    fn get_width(&self) -> u16 {
+        self.size * 2
+    }
+
+    fn get_height(&self) -> u16 {
+        self.size
+    }
+
+    pub fn calc(&self, area: Rect) -> Rect {
+        Rect {
+            x: area.x + self.pos[0],
+            y: area.y + self.pos[1],
+            width: self.get_width(),
+            height: self.get_height(),
         }
     }
 }
