908b60c998430d66e69cb6cadb534f1c15002b17
razigli <razigli@artix.dev>
2026-09-24 13:43:49 +0500

    create impl for Rat

--- src/rat.rs
+++ src/rat.rs
@@ -3,3 +3,9 @@ pub struct Rat {
     size: u16,
     field: [u16; 2],
 }
+
+impl Rat {
+    pub fn new(pos: [u16; 2], size: u16, field: [u16; 2]) -> Self {
+        Self { pos, size, field }
+    }
+}
