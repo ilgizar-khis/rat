5c339572d5e74918cb2083d0ef2d3985df180136
razigli <razigli@artix.dev>
2026-09-24 13:52:20 +0500

    rewrtie size and field logic

--- src/rat.rs
+++ src/rat.rs
@@ -6,6 +6,10 @@ pub struct Rat {
 
 impl Rat {
     pub fn new(pos: [u16; 2], size: u16, field: [u16; 2]) -> Self {
-        Self { pos, size, field }
+        Self {
+            pos: [pos[0] * 2, pos[1]],
+            size,
+            field: [field[0] * 2, field[1]],
+        }
     }
 }
